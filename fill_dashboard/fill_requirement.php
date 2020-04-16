18d7b2291814355e5ddd8de7856c5b5f4647770c
rivsgremxry <antylameer29@gmail.com>
2020-04-16 20:14:48 +0300

    Добавлена строка значений для функций

--- fill_dashboard/fill_requirement.php
+++ fill_dashboard/fill_requirement.php
@@ -19,7 +19,7 @@ function fill_requirement($conn)
 
     while ($row = mysqli_fetch_array($fill_requirement_result)) {
         $output .= '<div class="col-6" style="padding-top:10px; margin-bottom:13px;">';
-        $output .= '<input class="form-check-input" type="checkbox" id="' . $row["id"] . '" value="' . $row["requirement_title"] . '">';
+        $output .= '<input class="form-check-input" type="checkbox" id="' . $row["id"] . '" value="' . $row["id"] . '">';
         $output .= '<label class="form-check-label" for="' . $row["id"] . '">';
         $output .= $row["requirement_title"];
         $output .= '</label>';
--- load_data.php
+++ load_data.php
@@ -17,7 +17,7 @@ if(isset($_POST["id"]))
     while($row = mysqli_fetch_array($result))  
     {  
         $output .= '<div class="col-6" style="padding-top:10px; margin-bottom:13px;">';
-        $output .= '<input class="form-check-input" type="checkbox" id="' . $row["id"] . '">';
+        $output .= '<input class="form-check-input" type="checkbox" id="' . $row["id"] . '" value="' . $row["id"] . '">';
         $output .= '<label class="form-check-label" for="' . $row["id"] . '">';
         $output .= $row["requirement_title"];
         $output .= '</label>';
