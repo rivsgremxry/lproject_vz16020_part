06e97ce4272d8a95f153b5eca3aecf74373ce5d3
rivsgremxry <antylameer29@gmail.com>
2020-04-17 12:37:17 +0300

    Частично работающая функция update

--- modal_dialogs/save_changes.php
+++ modal_dialogs/save_changes.php
@@ -9,6 +9,14 @@ if (isset($_POST["requirements"])) {
         if (($req && $actor_id) != "") {
             echo "<script>console.log('requirement_id: " . $req . "' );</script>";
             echo "<script>console.log('actor_id: " . $actor_id . "' );</script>";
+
+            $sql = "UPDATE `requirement` SET `actor_id` = '$actor_id' WHERE `requirement`.`id` = $req;";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "<script>console.log('Record updated successfully!' );</script>";
+            } else {
+                echo "Error updating record: " . $conn->error;
+            }
         }
     }
 }
